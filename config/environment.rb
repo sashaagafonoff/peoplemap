fabcd713776a1a2d18b6bff0781fb898449eeede
sashaagafonoff <sasha.agafonoff@gmail.com>
2009-08-09 11:11:24 -0400

    Added installation notes for Ubuntu + Aptana Studio, made some small config changes to assist installation
    Signed-off-by: sashaagafonoff <sasha.agafonoff@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -30,6 +30,7 @@ Rails::Initializer.run do |config|
 
   config.frameworks -= [ :active_record ] #, :active_resource, :action_mailer ]
   config.gem "neo4j" #, :version => "0.3.0"
+  config.gem "haml"
   config.gem "calendar_date_select" 
   
 #  xml = File.read('config/entity_model.xml')
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,5 +16,6 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
+Lucene::Config[:store_on_file] = true 
 Lucene::Config[:storage_path] = 'db/lucene' 
 Neo4j::Config[:storage_path] = 'db/neo4j' 
